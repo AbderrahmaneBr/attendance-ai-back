015a42d5490122a3a68b708cd256252011a27264
Abderrahmane Bourri <abderrahmanebourri03@gmail.com>
2025-05-04 21:21:39 +0100

    init

---
+++ src/main/java/org/example/attendanceai/AttendanceAiApplication.java
@@ -0,0 +1,13 @@
+package org.example.attendanceai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttendanceAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/attendanceai/AttendanceAiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.attendanceai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
